f2eed2e462eb4c5b759162747a667ea948e16bf0
Melina Schild <melina.schild@icloud.com>
2023-12-29 17:47:15 +0100

    basic auth für addBoot & logout im frontend ergänzt

--- backend/user/controller.js
+++ backend/user/controller.js
@@ -3,19 +3,4 @@ export function userLogin(_, res) {
   // mit dem einfach success true weiß das FE bescheid, dass es username und password zu speichern sind (für zukünftige Abfragen)
 
   res.json({ success: true });
-
-  // oder ich gebe die fehler and frontend weiter (nicht wirklich, aber es geht):+
-  // if (res.locals.error) {
-  //   console.log(res.locals.error);
-  //   return res.status(401).json({
-  //     success: false,
-  //     error: 'Authentication failed',
-  //   });
-  // }
-
-  // res.status(200).json({
-  //   success: true,
-  //   message: 'Login successful',
-  // });
-  // console.log(res.locals.message);
 }
